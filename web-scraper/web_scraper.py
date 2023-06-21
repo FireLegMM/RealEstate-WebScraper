8aec09498894385b3dcb3d3f4605c58ebfa3cc52
FireLegMM <toniemojekonto@gmail.com>
2023-06-21 23:59:14 +0200

    update web_scraper

--- web-scraper/web_scraper.py
+++ web-scraper/web_scraper.py
@@ -5,10 +5,9 @@ import os
 from datetime import datetime
 import pathlib
 
-url = "https://www.otodom.pl/pl/oferty/sprzedaz/mieszkanie/powiat-wolominski?distanceRadius=0&locations=%5Bsubregions-200%5D&viewType=listing&limit=72"
+url = "https://www.otodom.pl/pl/wyniki/sprzedaz/mieszkanie/mazowieckie/wolominski?page=1&limit=72&distanceRadius=0&ownerTypeSingleSelect=ALL&by=DEFAULT&direction=DESC&viewType=listing&limit=72"
 data_path = pathlib.Path(str(pathlib.Path(__file__).parent.resolve()) + "/data/")
-data = requests.get(url)
-
+data = requests.get(url, allow_redirects=True, headers={"User-Agent": "Mozilla/5.0"})
 soup = bs(data.content, "html.parser")
 
 
@@ -19,8 +18,8 @@ page_count = data["props"]["pageProps"]["data"]["searchAds"]["pagination"]["tota
 dane = data["props"]["pageProps"]["data"]["searchAds"]["items"]
 
 for page in range(2, page_count + 1):
-    url = f"https://www.otodom.pl/pl/oferty/sprzedaz/mieszkanie/powiat-wolominski?distanceRadius=0&locations=%5Bsubregions-200%5D&viewType=listing&limit=72&page={str(page)}"
-    data = requests.get(url)
+    url = f"https://www.otodom.pl/pl/oferty/sprzedaz/mieszkanie/mazowieckie/wolominski?distanceRadius=0&locations=%5Bsubregions-200%5D&viewType=listing&limit=72&page={str(page)}"
+    data = requests.get(url, allow_redirects=True, headers={"User-Agent": "Mozilla/5.0"})
     soup = bs(data.content, "html.parser")
     soup_list = soup.find(type="application/json")
     data = json.loads(soup_list.text)
